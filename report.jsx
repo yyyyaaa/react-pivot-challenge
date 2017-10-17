068424a1ffdbfa0b0634141ac0bd197fccf547df
Phat G Ha <phatg@lecle.co.kr>
2017-10-17 22:16:40 +0700

    Fix js coding style

--- report.jsx
+++ report.jsx
@@ -7,52 +7,54 @@ var rows = require('./data.json')
 module.exports = createReactClass({
   render () {
     var dimensions = [
-      {value: 'date', title: "Date"},
-      {value: 'host', title: "Host"},
-    ];
+      {value: 'date', title: 'Date'},
+      {value: 'host', title: 'Host'}
+    ]
 
-    var reduce = function(row, memo) {
-      memo.impressions = (memo.impressions || 0) + (row.type === "impression" ? 1 : 0);
-      memo.loads = (memo.loads || 0) + (row.type === "load" ? 1 : 0);
-      memo.displays = (memo.displays || 0) + (row.type === "display" ? 1 : 0);
-      memo.loadRate = (memo.loads / memo.impressions) * 100;
-      memo.displayRate = (memo.displays / memo.loads) * 100;
-      return memo;
-    };
+    var reduce = function (row, memo) {
+      memo.impressions = (memo.impressions || 0) + (row.type === 'impression' ? 1 : 0)
+      memo.loads = (memo.loads || 0) + (row.type === 'load' ? 1 : 0)
+      memo.displays = (memo.displays || 0) + (row.type === 'display' ? 1 : 0)
+      memo.loadRate = (memo.loads / memo.impressions) * 100
+      memo.displayRate = (memo.displays / memo.loads) * 100
+      return memo
+    }
 
     var calculations = [
       {
-        title: 'Impressions', value: 'impressions',
+        title: 'Impressions', value: 'impressions'
       },
       {
-        title: 'Loads', value: 'loads',
+        title: 'Loads', value: 'loads'
       },
       {
-        title: 'Displays', value: 'displays',
-      }, 
+        title: 'Displays', value: 'displays'
+      },
       {
-        title: 'Load Rate', value: 'loadRate',
+        title: 'Load Rate',
+        value: 'loadRate',
         template: presentRate
-      }, 
+      },
       {
-        title: 'Display Rate', value: 'displayRate',
+        title: 'Display Rate',
+        value: 'displayRate',
         template: presentRate
       }
     ]
 
     return (
-      <div className="report">
+      <div className='report'>
         <ReactPivot rows={rows}
-                    dimensions={dimensions}
-                    reduce={reduce}
-                    calculations={calculations}
-                    activeDimensions={['Date', 'Host']}
+          dimensions={dimensions}
+          reduce={reduce}
+          calculations={calculations}
+          activeDimensions={['Date', 'Host']}
         />
       </div>
-    );
+    )
   }
 })
 
-function presentRate(value) {
-  return parseFloat(value).toFixed(1) + "%";
+function presentRate (value) {
+  return parseFloat(value).toFixed(1) + '%'
 }
